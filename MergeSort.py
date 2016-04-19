37154af67c78c279fe89a62507176e08a40acc39
Coder Jelly <coderjelly@users.noreply.github.com>
2016-04-19 11:01:10 -0700

    first commit

---
+++ MergeSort.py
@@ -0,0 +1,53 @@
+import sys
+
+def MergeSort(A,p,q):
+	if q > p:
+		mid = int((p + q)/2)
+		MergeSort(A,p,mid)
+		MergeSort(A,mid+1,q)
+		Merge(A,p,mid,q)
+
+def Merge(A,p,mid,q):
+
+	a = 0
+	b = 0
+	i = p
+
+	L = A[p:mid+1]
+	R = A[mid+1:q]
+
+	maxL = len(L)
+	maxR = len(R)
+
+	L.append(sys.maxint)
+	R.append(sys.maxint)
+
+	while a < maxL and b < maxR:
+
+		if L[a] < R[b]:
+			A[i] = L[a]
+			a += 1
+
+		else:
+			A[i] = R[b]
+			b += 1
+
+		i += 1
+
+	while a < maxL:
+		A[i] = L[a]
+		a += 1
+		i += 1
+
+	while b < maxR:
+		A[i] = R[b]
+		b += 1
+		i += 1
+
+k = [1,2,7,1,9,-3,2,0]
+
+print k
+
+MergeSort(k,0,len(k))
+
+print k
---
+++ NodeClass.py
@@ -0,0 +1,44 @@
+
+class Node:
+	'basic Node definition'
+	totalNodes = 0
+
+	def __init__(self,value=0):
+		self.left = None
+		self.right = None
+		self.value = value
+		Node.totalNodes += 1
+		print "\n#######"
+		print "Node created, with value = %d" % value
+
+	def printNode(self):
+		print "\n_____________\n"
+		print "Node value = " + str(self.value)
+		if self.right is None:
+			print "Right = None" 
+		else:
+			print "Right = " + str(self.right.value)
+		if self.left is None:
+			print "Left = None" 
+		else:
+			print "Left = " + str(self.left.value)
+
+
+
+n1 = Node(4)
+n1.printNode()
+
+n2 = Node(7)
+n2.printNode()
+
+n3 = Node(8)
+n3.printNode()
+
+n3 = Node()
+n3.printNode()
+
+n1.right = n2
+n1.left = n3
+n1.printNode()
+
+print Node.totalNodes
